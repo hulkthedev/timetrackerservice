db2ea13708301bd8320e1cb27820921645ef792c
Alexej Beirith <alexej.beirith@arvato.com>
2020-02-06 16:28:21 +0100

    add new sql structure

--- config/mariadb/migrate/01_init.sql
+++ config/mariadb/migrate/01_init.sql
@@ -1,38 +1,51 @@
 CREATE TABLE employer (
     id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    working_hours_half TINYINT NOT NULL DEFAULT 6,
-    working_break_half TINYINT NOT NULL DEFAULT 0,
-    working_hours_full TINYINT NOT NULL DEFAULT 8,
-    working_break_full TINYINT NOT NULL DEFAULT 30,
-    working_hours_extreme TINYINT NULL,
-    working_break_extreme TINYINT NULL,
-    vacation_days TINYINT NOT NULL DEFAULT 30,
+    working_hours_half TINYINT UNSIGNED NOT NULL DEFAULT 6,
+    working_break_half TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    working_hours_full TINYINT UNSIGNED NOT NULL DEFAULT 8,
+    working_break_full TINYINT UNSIGNED NOT NULL DEFAULT 30,
+    working_hours_extreme TINYINT UNSIGNED NULL,
+    working_break_extreme TINYINT UNSIGNED NULL,
+    vacation_days TINYINT UNSIGNED NOT NULL DEFAULT 30,
 
-    CONSTRAINT unique_employer UNIQUE (name),
+    CONSTRAINT unique_employer UNIQUE (name)
 )
 CHARACTER SET 'utf8mb4' COLLATE = 'utf8mb4_unicode_ci';
 
 CREATE TABLE employer_config (
     id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     employer_id SMALLINT UNSIGNED NOT NULL,
-    employer_color VARCHAR(7) NOT NULL DEFAULT '#000000',
-    time_account BIT NOT NULL 1,
+    frontend_color VARCHAR(7) NOT NULL DEFAULT '#000000',
 
+    CONSTRAINT unique_employer UNIQUE (employer_id),
     CONSTRAINT fk_employer FOREIGN KEY (employer_id) REFERENCES employer (id)
         ON DELETE CASCADE
         ON UPDATE RESTRICT
 )
 CHARACTER SET 'utf8mb4' COLLATE = 'utf8mb4_unicode_ci';
 
-CREATE TABLE timetracking (
+CREATE TABLE time_account (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    employer_id SMALLINT UNSIGNED NULL,
+    enabled BIT NOT NULL DEFAULT  1,
+    time SMALLINT NULL DEFAULT 0,
+
+    CONSTRAINT unique_employer UNIQUE (employer_id),
+    CONSTRAINT fk_employer FOREIGN KEY (employer_id) REFERENCES employer (id)
+        ON DELETE CASCADE
+        ON UPDATE RESTRICT
+)
+CHARACTER SET 'utf8mb4' COLLATE = 'utf8mb4_unicode_ci';
+
+CREATE TABLE working_times (
     id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     employer_id SMALLINT UNSIGNED NOT NULL,
     date DATE NOT NULL,
     mode VARCHAR(20) NOT NULL,
     begin_timestamp INT UNSIGNED NOT NULL,
     end_timestamp INT UNSIGNED NOT NULL DEFAULT 0,
-    delta SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+    delta SMALLINT NOT NULL DEFAULT 0,
 
     CONSTRAINT unique_days UNIQUE (date, employer_id, mode),
     CONSTRAINT fk_employer FOREIGN KEY (employer_id) REFERENCES employer (id)
